25b200d9206729e947229623b75034e9deeeaae4
Mesup Adhikari <72329116+iammesup@users.noreply.github.com>
2020-10-23 06:55:48 -0400

    Update For loops in python.py

--- For loops in python.py
+++ For loops in python.py
@@ -34,3 +34,59 @@ for items,lolly in dict3.items():
 #if we want only the keys of dictionary then we can write 
 for items in dict3:
     print(items)
+#if we want to print the letters in words then we simply write
+for x in "banana":
+  print(x)
+#to print the no upto (what you write)
+for x in range(6):
+  print(x)
+"""
+output is 
+0
+1
+2
+3
+4
+5
+"""
+#if we want to print the no inside(,)
+for x in range(2, 6):
+  print(x)
+"""
+output is 
+3
+4
+5
+"""
+#if we want gap between no then we simply print(starting no,ending no,gap no)
+for x in range(2, 30, 3):
+  print(x)
+"""
+output is 
+2
+5
+8
+11
+14
+17
+20
+23
+26
+29
+"""
+#Print all numbers from 0 to 5, and print a message when the loop has ended:
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!") 
+#Print each adjective for every fruit:
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+  for y in fruits:
+    print(x, y) 
+#for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error.
+#having an empty for loop like this, would raise an error without the pass statement
+for x in [0, 1, 2]:
+  pass
+
