6a2ab5f060309a4bc596c2858129cf876e531bd8
Mesup Adhikari <72329116+iammesup@users.noreply.github.com>
2020-10-23 07:23:57 -0400

    Add files via upload

---
+++ Whille loops in python.py
@@ -0,0 +1,28 @@
+"""
+Python has two primitive loop commands:
+    while loops
+    for loops
+"""
+"""
+The while Loop
+With the while loop we can execute a set of statements as long as a condition is true."""
+"""in for loop it repeat untill the content will complete in the list,tuple or set
+but while loop repeat untill the condition is true."""
+"""
+The while loop requires relevant variables to be ready, in this example we need to define an indexing variable, i, which we set to 0
+"""
+"""the syntax is written as while(condition):"""
+#for example:while(i<45):
+"""
+i=0
+while(i<45):
+    print(i)#it never stop it runs.......
+"""
+i=0
+while(i<45):#it print upto 44 and then stops because the condition will become false 
+    print(i)
+    i=i+1
+
+
+
+
