86218034abb817dd75fc8ef7fc5367b57045843b
Mesup Adhikari <72329116+iammesup@users.noreply.github.com>
2020-10-25 23:05:03 -0400

    Add files via upload

---
+++ Short Hand If Else Notation In Python.py
@@ -0,0 +1,15 @@
+a=int(input("enter a\n"))
+b=int(input("enter b\n"))
+'''
+if a>b: print("a b se bada ha bhai")#one line print and readable code.
+if a>b:
+    print("a b se bada ha bhai")#two line print 
+'''
+"""
+if a<b:
+    print("b a se bada ha bhai")
+else:
+    print("a b se bada ha bhai")
+"""
+
+print("b a se bada ha bhai") if a<b else print("a b se bada ha bhai")#one line code.
