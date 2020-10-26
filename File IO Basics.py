92a92fc0b715926c0a1536b19746d716d11a6fa0
Mesup Adhikari <72329116+iammesup@users.noreply.github.com>
2020-10-26 01:03:29 -0400

    Add files via upload

---
+++ File IO Basics.py
@@ -0,0 +1,15 @@
+# File IO Basics
+'''
+Harddisk-non volatile memory(the data will store when computer is closed.)
+Ram-volatile memory(lost of data when computer is closed.)
+'''
+'''
+"r"-Open file for reading
+"w"-Open file for writing
+"x"-Create a file if not exits
+"a"-(Append)Add more content to a file(last)
+"t"-Text mode(python file,txt file,notepad etc.)
+"b"-Binary mode
+"+"-read and write 
+
+'''
