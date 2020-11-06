06981465d87b30d6821bc9fb1609efb0522d5f99
Mesup Adhikari <72329116+iammesup@users.noreply.github.com>
2020-11-06 16:57:35 +0545

    Add files via upload

---
+++ join_function.py
@@ -0,0 +1,16 @@
+list1=["mesup","join","wwe","khali"]
+# if we want to write mesup or join or.............
+# using simple method
+for item in list1:
+    print(item ,"or", end=" ")
+print("hello")
+# using join function
+bps=["hello","mesup","bala"]
+a=" or ".join(bps)
+print(a," hello ")
+# using ,
+bps=["hello","mesup","bala"]
+a=" , ".join(bps)
+print(a," hello ")
+
+
